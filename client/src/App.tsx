acaae2c5300546d3b422a31d8080b1e723db14e6
daniel amekpoagbe <amekpoagbedaniel@gmail.com>
2025-06-20 02:36:48 +0000

    added frontend files

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-green-500 font-bold text-center mt-40 text-7xl  ">
+      {" "}
+      Hello tailwind test
+    </div>
+  );
+}
+
+export default App;
