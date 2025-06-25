c44f2d2c667373d97560e47828524b21840c4aa0
Sakyi Mustapha <sakyimustapha5@gmail.com>
2025-06-25 10:18:18 +0000

    Add server folder and files

--- server/app.py
+++ server/app.py
@@ -30,7 +30,7 @@ def register():
         data = request.get_json()
         firstname = data.get('fname')
         lastname = data.get('lname')
-        email = data.get('email')
+        email = data.get('email') 
         password = data.get('password')
 
 
@@ -47,7 +47,8 @@ def register():
             Missing_fields.append(password)
             return jsonify({"Error": f"Missing_fields: {Missing_fields}"}), 400
 
-    
+        if (firstname and lastname and email and password):
+            return jsonify({'message': 'successfully signed up'}), 201
 
         #Check if email already exists
         existing_user = Buyer_info.query.filter_by(email=email).first()
@@ -66,8 +67,7 @@ def register():
         db.session.commit()
    
     
-        if (firstname and lastname and email and password):
-            return jsonify({'message': 'successfully signed up'}), 201
+        
     
 
 
@@ -89,12 +89,12 @@ def login():
         password = data.get('password')
 
 
-        # Step 1: Find user by email
+        #find a user by email 
         user = Buyer_info.query.filter_by(email=email).first()
 
-        # Step 2: If user exists and password matches
+        #If user exists and password matches
         if user and check_password_hash(user.password, password):
-            return jsonify({"message": "Successfully logged in"}), 200
+            return jsonify({"message": "Successfully logged in"}), 201
 
         if not (firstname and lastname and email and password):
             return jsonify({"message": "Invalid credentials"}), 401
@@ -111,9 +111,104 @@ def login():
             Missing_fields.append('password')
             return jsonify({f"Missing_fields": {Missing_fields}},"fill in all blank spaces"), 400
     
+
+    
+
+
+
+#DATABASE FOR BUSINESS PROFILE 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///business.db'
+
+class Business(db.Model):
+    id = db.Column(db.Integer, primary_key=True)    
+    business_name = db.Column(db.String(50))
+    business_email = db.Column(db.String(150), unique=True,)
+    phone = db.Column(db.String(150),)
+
+
+#Signup form for businesses
+@app.route('/business', methods=['POST'])
+def business_user():
+    data = request.get_json()
+    business_name = data.get('business_name')
+    business_email = data.get('business_email')
+    phone = data.get('phone')
+
+
+
+    Missing_fields= []
+
+    if not business_name:
+        Missing_fields.append('business_name')
+    if not business_email:
+        Missing_fields.append('business_email')
+    if not phone:
+        Missing_fields.append('phone')
+        
+        
+    
+    if Missing_fields:
+        return jsonify({"Error": f"Missing_fields: {Missing_fields}"}), 400
+     
+
+
+    #saves products/items into the database
+    new_business = Business(business_name=business_name, business_email=business_email, phone=phone,)
+    db.session.add(new_business)
+    db.session.commit()
+
+
+
+    if (business_name and business_email and phone):
+        return jsonify({'message': 'Information uploaded successfully'})
+    else:
+        return jsonify({'message': "Error, can't upload information"})
+    
+#LOGIN form for business
+@app.route('/getbusiness', methods=['POST'])
+
+def getbusiness():
+     #get data from the database
+     data = request.get_json()
+     business_name = data.get('business_name')
+     business_email = data.get('business_email')
+     phone = data.get('phone')
+
+
+     Missing_fields = []
+     if not business_name:
+            Missing_fields.append('business_name')
+     if not business_email:
+            Missing_fields.append('business_email')
+     if not phone:
+            Missing_fields.append('phone')
+
+
+     if Missing_fields:
+          return jsonify({f"Missing_fields", Missing_fields,"'message': fill in all blank spaces"}), 400
+    
+            
+
+
+
+
+    #Helps to find user by email or phone
+     business_user = Business.query.filter_by(
+          business_email=business_email, phone=phone).first()
+     
+     
+     #conditional statements
+     if   business_user:
+          return jsonify({'message': 'Information retrieved successfully'}), 201
+     else:
+          return jsonify({'message': 'Error, information could not be retrieved'}), 404
+     
+     
+     
+    
 #PRODUCT CODE BASE
 #product database structure
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///product.db'
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///product.db'                                           
 
 class products(db.Model):
         id = db.Column(db.Integer, primary_key=True)    
@@ -125,24 +220,25 @@ class products(db.Model):
 @app.route('/product', methods=['POST'])
 def product():
         data = request.get_json()
-        product_name = data.get('pro_name')
-        product_price = data.get('pro_price')
-        product_uses = data.get('pro_uses')
-        product_images = data.get('pro_images')
+        product_name = data.get('product_name')
+        product_price = data.get('product_price')
+        product_uses = data.get('product_uses')
+        product_images = data.get('product_images')
 
         Missing_fields= []
 
         if not product_name:
-            Missing_fields.append(product_name)
+            Missing_fields.append('product_name')
         if not product_price:
-            Missing_fields.append(product_price)
+            Missing_fields.append('product_price')
         if not product_uses:
-            Missing_fields.append(product_uses)
+            Missing_fields.append('product_uses')
         if not product_images:
-            Missing_fields.append(product_images)
-            return jsonify({"Error": f"Missing_fields: {Missing_fields}"}), 400
+            Missing_fields.append('product_images')
+            
 
-    
+        if Missing_fields:
+             return jsonify({"Error": f"Missing_fields: {Missing_fields}"}), 400
 
         
        #saves products/items into the database
@@ -150,15 +246,14 @@ def product():
         db.session.add(new_product)
         db.session.commit()
 
+        
+             
        
-       
-        if (product_name and product_price and product_uses and product_images):
+        if product:
              return jsonify({'message': 'Item updated successfully '}), 201
         
-        if product in (product_name and product_price and product_uses and product_images):
-             return jsonify({'message': 'Item already in stock'})
         
-        if not (product_name and product_price and product_uses and product_images):
+        if not product:
              return jsonify({'message': 'Error item not updated'}), 400
 
         
@@ -168,13 +263,6 @@ def product():
 @app.route('/getproduct', methods=['GET'])
 def getproduct():
      
-    data = ({'product':
-        'product_name'
-        'product_price'
-        'product_uses'
-        'product_images'
-     
-})
     data = request.get_json()
     product_name = data.get('product_name')
     product_price = data.get('product_price')
@@ -183,116 +271,31 @@ def getproduct():
 
     
     
-    if  (product_name and product_price and product_uses and product_images):
-         return jsonify({'message': 'items retrieved successfully'}), 201
-    if not (product_name and product_price and product_uses and product_images):
-         return jsonify({'message': 'could not retrieve item'}), 400
-    
     Missing_fields= []
 
     if not product_name:
-            Missing_fields.append(product_name)
+            Missing_fields.append('product_name')
     if not product_price:
-            Missing_fields.append(product_price)
+            Missing_fields.append('product_price')
     if not product_uses:
-            Missing_fields.append(product_uses)
+            Missing_fields.append('product_uses')
     if not product_images:
-            Missing_fields.append(product_images)
-            return jsonify({"Error": f"missing_fields: {Missing_fields}"}), 400
-
-    
+            Missing_fields.append('product_images')
 
 
 
-#DATABASE FOR BUSINESS PROFILE 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///business.db'
-
-class Business_info(db.Model):
-    id = db.Column(db.Integer, primary_key=True)    
-    Business_name = db.Column(db.String(50))
-    Business_email = db.Column(db.String(150), unique=True,)
-    phone = db.Column(db.String(150),)
-
-
-#Signup form for businesses
-@app.route('/business', methods=['POST'])
-def business():
-    data = request.get_json()
-    business_name = data.get('Business_name')
-    business_email = data.get('Business_email')
-    phone = data.get('phone')
-
-
-
-    Missing_fields= []
-
-    if not business_name:
-        Missing_fields.append(business_name)
-    if not business_email:
-        Missing_fields.append(business_email)
-    if not phone:
-        Missing_fields.append(phone)
-        return jsonify({"Error": f"Missing_fields: {Missing_fields}"}), 400
-     
-
-
-    #saves products/items into the database
-    new_business = business(Business_name=business_name, Business_email=business_email, phone=phone,)
-    db.session.add(new_business)
-    db.session.commit()
-
-
-
-    if (business_name and business_email and phone):
-        return jsonify({'message': 'Information uploaded successfully'})
-    if not (business_name and business_email and phone):
-        return jsonify({'message': "Error, can't upload information"})
-
-
-#LOGIN form for business
-@app.route('/getbusiness', methods=['POST'])
-def getbusiness():
-     
-     data = {'business':
-             'business_name'
-             'business_email'
-             'phone'
-          
-     }
-     #get data from the database
-     data = request.get_json()
-     business_name = data.get('bsiness_name')
-     business_email = data.get('business_email')
-     phone = data.get('phone')
-
-     user = Business_info.query.filter_by(email=business_email).first()
-     
-     
-     #conditional statements
-     if (business_name and business_email and phone):
-          return jsonify({'message': 'Information retrieved successfully'})
-     if not (business_name and business_email and phone):
-          return jsonify({'message': 'Error, information could not be retrieved'})
-     
-     
-     Missing_fields = []
-     if not business_name:
-            Missing_fields.append('Business_name')
-     if not business_email:
-            Missing_fields.append('Business_email')
-     if not phone:
-            Missing_fields.append('phone')
-            return jsonify({f"Missing_fields": {Missing_fields}},"fill in all blank spaces"), 400
-    
-
-
-
-
-     
-
+    if Missing_fields:
+        return jsonify({"Error": f"missing_fields: {Missing_fields}"}), 400
 
         
+   
 
+    if product:
+         return jsonify({'message': 'items retrieved successfully'}), 201
+    if not product:
+         return jsonify({'message': 'could not retrieve item'}), 400
+
+    
 
     
 #this allows sqlalchemy to find data and create data according to your db.Model
