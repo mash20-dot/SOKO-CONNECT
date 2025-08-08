4d3f6647624063169c770a746c17adf07b603038
Sakyi Mustapha <sakyimustapha5@gmail.com>
2025-08-08 23:42:35 +0000

    allowed buyers to update their orders and delete orders, with some bug

--- server/order_pro/order_item.py
+++ server/order_pro/order_item.py
@@ -112,4 +112,61 @@ def stream_orders():
     return Response(stream_order_updates(), mimetype='text/event-stream')
 
 
-#HANDLE WHERE A USER CAN RETRIEVE ONLY WHAT HE BOUGHT NOT WHAT OTHERS BOUGHT AS WELL
+@order_pro.route('/update_order', methods=['PUT'])
+@jwt_required()
+@role_required("user")
+def update_order():
+
+     current_email = get_jwt_identity()
+     change_order = Orders.query.filter_by(tracking_code=tracking_code).first()
+
+     if not current_email:
+          return jsonify({"message": "user not found"}), 400
+     
+     data = request.get_json()
+     new_product = data.get("product")
+     tracking_code = data.get("old_tracking")
+     
+     if not new_product or not tracking_code:
+          return jsonify({"message": "old_tracking and product required"}), 401
+     
+     if not (change_order.tracking_code,  tracking_code):
+          return jsonify({"message": "tracking code is invalid"}), 400
+     
+     change_order.product = new_product
+     db.session.commit()
+     return jsonify({"message": "update made successfullt"}), 201
+     
+     #TEST THIS ROUTE AND ADD A ROUTE TO DELETE AN ORDER
+
+@order_pro.route('/delete_order', methods=['DELETE'])
+@jwt_required()
+@role_required("user")
+def delete_order():
+
+     current_email = get_jwt_identity()
+     user =    Buyer_user.query.filter_by(email=current_email).first()
+     
+     if not user:
+          return jsonify({"message": "no order was found"}), 400
+     
+     
+     data= request.get_json()
+     your_tracking_code = data.get("your_tracking_code")
+     del_pro = data.get("del_pro")
+
+     
+
+     if not your_tracking_code or not del_pro:
+          return jsonify({"message": "del_pro and tracking code required"}), 401
+     
+     track = Orders.query.filter_by(tracking_code=your_tracking_code).first()
+
+     if not (track.tracking_code, your_tracking_code):
+          return jsonify({"message": "Invalid tracking code"}), 400
+
+     
+     track.product = del_pro
+     db.session.delete(track)
+     db.session.commit()
+     return jsonify({"message": f"Order {track} deleted successfully"}), 201
